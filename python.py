d526b621ff787b18435df56732e79fbdffeedfad
Li yongxiao <132033941+liyongxiaoOUO@users.noreply.github.com>
2023-04-28 19:40:57 +0800

    Add files via upload

---
+++ python.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 28 19:06:37 2022
+
+@author: pierre
+"""
+
+input_sentence = input("input the sentence: \n")
+word_list = input_sentence.split()
+print(f"this list has {len(word_list)} elements : ")
+for i in range(len(word_list)):
+    print(f"{i:<3d}:{word_list[i]}")
+print("------------------")
+delete_element_num = input("which one to be deleted: ")
+word_list.remove(word_list[int(delete_element_num)])
+result = " "
+result = result.join(word_list)
+print(f"Result : {result}")
